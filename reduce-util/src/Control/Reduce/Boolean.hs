79ed24de9b862001b9fdc30a50459d9ec2acb5e9
Christian Gram Kalhauge <christian@kalhauge.dk>
2019-11-04 15:35:42 -0800

    Rewriting Boolean Library

--- reduce-util/src/Control/Reduce/Boolean.hs
+++ reduce-util/src/Control/Reduce/Boolean.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
@@ -13,13 +23,17 @@ module Control.Reduce.Boolean where
 import Prelude hiding (not, and, or)
 import qualified Prelude
 import Data.Monoid
-import Data.Foldable (toList)
-import qualified Data.List as List
-import Data.Bits
-import Data.Int
+import Data.Coerce
+-- import Data.Foldable (toList)
+-- import qualified Data.List as List
+-- import Data.Bits
+-- import Data.Int
+
+-- import Data.Bifoldable
+-- import Data.Bitraversable
 
 -- containers
-import qualified Data.IntSet as IS
+-- import qualified Data.IntSet as IS
 
 -- vector
 --import qualified Data.Vector.Unboxed as UV
@@ -36,8 +50,14 @@ class Boolean a where
   true :: a
   false :: a
   not :: a -> a
-
   a ==> b = not a \/ b
+
+(∧) :: Boolean a => a -> a -> a
+(∧) = (/\)
+(∨) :: Boolean a => a -> a -> a
+(∨) = (\/)
+infixl 5 ∧
+infixl 4 ∨
 
 -- | A synonym for 'not'
 neg :: Boolean a => a -> a
@@ -50,16 +70,22 @@ instance Boolean Bool where
   true = True
   false = False
 
-newtype And a = And { runAnd :: a }
-  deriving (Show, Boolean)
-newtype Or a = Or { runOr :: a }
-  deriving (Show, Boolean)
+class BooleanAlgebra a where
+  type BooleanVar a
+  tt :: BooleanVar a -> a
+  ff :: BooleanVar a -> a
 
-instance Boolean a => Semigroup (And a) where (<>) = (/\)
-instance Boolean a => Monoid (And a) where mempty = true
 
-instance Boolean a => Semigroup (Or a) where (<>) = (\/)
-instance Boolean a => Monoid (Or a) where mempty = false
+-- newtype And a = And { runAnd :: a }
+--   deriving (Show, Boolean)
+-- newtype Or a = Or { runOr :: a }
+--   deriving (Show, Boolean)
+
+-- instance Boolean a => Semigroup (And a) where (<>) = (/\)
+-- instance Boolean a => Monoid (And a) where mempty = true
+
+-- instance Boolean a => Semigroup (Or a) where (<>) = (\/)
+-- instance Boolean a => Monoid (Or a) where mempty = false
 
 and :: (Foldable f, Boolean a) => f a -> a
 and = andOf folded
@@ -96,152 +122,426 @@ existOf l a fn = orOf (l.to fn) a
 given :: Boolean b => Bool -> b -> b
 given b m = if b then m else true
 
+data Fix f = Fix { unFix :: f (Fix f) }
+
+deriving instance Eq (f (Fix f)) => Eq (Fix f)
+deriving instance Ord (f (Fix f)) => Ord (Fix f)
+deriving instance Show (f (Fix f)) => Show (Fix f)
+
+class Functor f => Fixed f b | b -> f where
+
+  cata :: (f a -> a) -> b -> a
+  default cata :: Coercible (Fix f) b => (f a -> a) -> b -> a
+  cata f = f . fmap (cata f) . unFix . coerce
+
+  ana  :: (a -> f a) -> a -> b
+  default ana :: Coercible b (Fix f) => (a -> f a) -> a -> b
+  ana f = coerce . Fix . fmap (ana f) . f
+
+liftF :: (Coercible b (Fix f), Coercible (Fix f) b, Functor f) => f b -> b
+liftF = coerce . Fix . fmap coerce
+
+unliftF :: (Coercible b (Fix f), Coercible (Fix f) b, Functor f) => b -> f b
+unliftF = fmap coerce . unFix . coerce
+
+hylo :: forall f y x. (Fixed f x, Fixed f y) => (f y -> y) -> (x -> f x) -> x -> y
+hylo phi psi x = cata phi (ana psi x :: y)
+
+instance Functor f => Fixed f (Fix f) where
+
 -- | Term is a free boolean structure with variables.
-data Term a
-  = TAnd (Term a) (Term a)
-  | TOr  (Term a) (Term a)
-  | TNot (Term a)
+data TermF a f
+  = TAnd f f
+  | TOr  f f
+  | TNot f
   | TVar !a
   | TConst !Bool
-  deriving (Show, Functor, Foldable)
+  deriving
+    ( Eq, Ord
+    , Functor, Foldable, Traversable
+    )
+
+instance Bifunctor TermF where
+  bimap f s = \case
+    TAnd a b -> TAnd (s a) (s b)
+    TOr  a b -> TOr (s a) (s b)
+    TNot a   -> TNot (s a)
+    TVar a   -> TVar (f a)
+    TConst b -> TConst b
+
+-- instance Bifoldable TermF where
+-- instance Bitraversable TermF where
+ 
+newtype Term a = Term (Fix (TermF a))
+  deriving (Eq, Ord)
+
+-- deriving Functor Term where
+--   fmap f =
+
+
+instance Fixed (TermF a) (Term a)
 
 instance Boolean (Term a) where
-  (/\)  = TAnd
-  (\/)  = TOr
-  not a = TNot a
-  true  = TConst True
-  false = TConst False
+  (/\) a b = liftF (TAnd a b)
+  (\/) a b = liftF (TOr a b)
+  not   = liftF . TNot
+  true  = liftF (TConst True)
+  false = liftF (TConst False)
   {-# INLINE (/\) #-}
   {-# INLINE (\/) #-}
   {-# INLINE not #-}
+  {-# INLINE false #-}
+  {-# INLINE true #-}
+
+instance BooleanAlgebra (Term a) where
+  type BooleanVar (Term a) = a
+  tt a = liftF (TVar a)
+  ff a = neg (liftF (TVar a))
+
+showsPrecTermF :: Show a => TermF a (Int -> ShowS) -> Int -> ShowS
+showsPrecTermF = \case
+  TAnd a b -> \n -> showParen (n > 5) (a 5 . showString " ∧ " . b 6)
+  TOr a b  -> \n -> showParen (n > 4) (a 4 . showString " ∨ " . b 5)
+  TNot a -> \n -> showParen (n > 9) (showString "not " . a 10)
+  TVar i -> \n -> showParen (n > 9) (showString "tt " . showsPrec 10 i)
+  TConst True -> const $ showString "true"
+  TConst False -> const $ showString "false"
+
+instance Show a => Show (Term a) where
+  showsPrec n f = cata showsPrecTermF f n
+
+toTerm :: Fixed (TermF a) x => x -> Term a
+toTerm = cata liftF
+{-# INLINE toTerm #-}
+
+fromTerm :: Fixed (TermF a) x => Term a -> x
+fromTerm = ana unliftF
+{-# INLINE fromTerm #-}
+
+crossCompiler :: (Fixed (TermF a) x, Fixed (TermF a) y) => x -> y
+crossCompiler = fromTerm . toTerm
 
 
--- | We are currently operating at 31 bits.
-type VarId = Int32
+-- | A literal is either true or false.
+data Literal = Literal {-# UNPACK #-} !Bool {-# UNPACK#-} !Int
+  deriving (Eq, Ord, Show)
+
+instance BooleanAlgebra Literal where
+  type BooleanVar Literal = Int
+  tt = Literal True
+  ff = Literal False
+
 
 -- | Negation normal form
 data Nnf
   = NAnd Nnf Nnf
   | NOr Nnf Nnf
-  | NVar !Bool !Int
+  | NLit {-# UNPACK #-} !Literal
   | NConst !Bool
+  deriving (Eq)
 
-nnfCompiler :: Term Int -> Nnf
-nnfCompiler = go True where
-  go n = \case
-    TAnd t1 t2 ->
-      (if n then NAnd else NOr) (go n t1) (go n t2)
-    TOr  t1 t2 ->
-      (if n then NOr else NAnd) (go n t1) (go n t2)
-    TNot t1 ->
-      go (not n) t1
-    TVar a ->
-      NVar n a
-    TConst b ->
-      NConst b
-{-# INLINE nnfCompiler #-}
+instance Fixed (TermF Int) Nnf where
+  -- cata :: (TermF Int a -> a) -> b -> a
+  cata fn = \case
+    NAnd a b ->
+      fn $ TAnd (cata fn a) (cata fn b)
+    NOr a b ->
+      fn $ TOr  (cata fn a) (cata fn b)
+    NLit (Literal True i) ->
+      fn $ TVar i
+    NLit (Literal False i) ->
+      fn $ TNot (fn $ TVar i)
+    NConst b ->
+      fn $ TConst b
 
--- | Split a variable into it's components
-splitVar :: VarId -> (Bool, Int32)
-splitVar i = (not $ testBit i 31, clearBit i 31)
-{-# INLINE splitVar #-}
+  -- ana  :: (a -> TermF Int a) -> a -> b
+  ana fn = go True . fn where
+    go n = \case
+      TAnd t1 t2 ->
+        (if n then NAnd else NOr) (go n $ fn t1) (go n $ fn t2)
+      TOr  t1 t2 ->
+        (if n then NOr else NAnd) (go n $ fn t1) (go n $ fn t2)
+      TNot t1 ->
+        go (not n) $ fn t1
+      TVar a ->
+        NLit (Literal n a)
+      TConst b ->
+        NConst (if n then b else not b)
 
--- | Split a variable into it's components
-joinVar :: (Bool, Int32) -> VarId
-joinVar (b, i) = if b then clearBit i 31 else setBit i 31
-{-# INLINE joinVar #-}
+instance Show Nnf where
+  showsPrec n f = cata showsPrecTermF f n
 
-notvar :: VarId -> VarId
-notvar i = complementBit i 31
-{-# INLINE notvar #-}
+instance Boolean Nnf where
+  (/\)  = NAnd
+  (\/)  = NOr
+  not   = fromTerm . neg . toTerm
+  true  = NConst True
+  false = NConst False
+  {-# INLINE (/\) #-}
+  {-# INLINE (\/) #-}
+  {-# INLINE not #-}
 
--- | A Clause is a unboxed sorted vector of 'Var's.
-data Clause = Clause IS.IntSet IS.IntSet
-  deriving (Eq, Ord)
+instance BooleanAlgebra Nnf where
+  type BooleanVar Nnf = Int
+  tt = NLit . tt
+  ff = NLit . ff
 
-
-orVid :: Bool -> Int -> Clause -> Clause
-orVid b i (Clause trs fls)
-  | b =
-    Clause (i `IS.insert` trs) fls
-  | otherwise =
-    Clause trs (i `IS.insert` fls)
-
-
-
-instance Show Clause where
-  showsPrec n (Clause tr fl) =
-    showParen (n > 9) $
-    showString "clause "
-    . showList (
-    map (True,) (IS.toList tr)
-    ++ map (False,) (IS.toList fl)
-    )
-
-clause :: (Foldable f) => f (Bool, Int) -> Clause
-clause (toList -> f) =
-  Clause
-  (IS.fromList . map snd $ trues)
-  (IS.fromList . map snd $ falses)
-  where
-    (trues, falses) = List.partition fst f
-{-# INLINE clause #-}
-
-negateClause :: Clause -> [Clause]
-negateClause (Clause tr fl) =
-  [ Clause mempty (IS.singleton i) | i <- IS.toList tr]
-  ++ [ Clause (IS.singleton i) mempty | i <- IS.toList fl]
-{-# INLINE negateClause #-}
-
-joinClause :: Clause -> Clause -> Clause
-joinClause (Clause trs1 fls1) (Clause trs2 fls2) =
-  Clause
-  (IS.union trs1 trs2)
-  (IS.union fls1 fls2)
-{-# INLINE joinClause #-}
-
-flattenClause :: Clause -> Maybe Clause
-flattenClause a@(Clause tr fl) =
-  if tr `IS.disjoint` fl
-  then Just a
-  else Nothing
-
--- | A CNF is a list of clauses
-type Cnf = [Clause]
-
--- instance Boolean Cnf where
---   a /\ b = a ++ b
-
---   (\/) = liftM2 joinClause
-
---   not = \case
---     [] -> false
---     c : rest ->
---      negateClause c \/ not rest
-
---   true  = []
---   false = [clause []]
-
-cnfCompiler :: Term Int -> Cnf
-cnfCompiler =
-  flip appEndo [] . go (Clause mempty mempty) (Endo . (:)). nnfCompiler where
-  -- go: c is the dominating clause. Every clause have to be or'ed with
-  -- this. This is a fold.
-  go c k = \case
-    NAnd t1 t2   -> go c k t1 <> go c k t2
-    NOr t1 t2    -> go c (\c' -> go c' k t2) t1
-    NVar b i     -> k (orVid b i c)
-    NConst True  -> mempty
-    NConst False -> k (c)
+-- | Flattens an nnf
+flattenNnf :: Nnf -> Nnf
+flattenNnf = \case
+  NAnd a b ->
+    case (flattenNnf a, flattenNnf b) of
+      (NConst True, t)  -> t
+      (t, NConst True)  -> t
+      (NConst False, _) -> NConst False
+      (_, NConst False) -> NConst False
+      (a', b') -> NAnd a' b'
+  NOr a b ->
+    case (flattenNnf a, flattenNnf b) of
+      (NConst True, _)  -> NConst True
+      (_, NConst True)  -> NConst True
+      (NConst False, t) -> t
+      (t, NConst False) -> t
+      (a', b') -> NOr a' b'
+  x -> x
 
 
--- go where
---   go = \case
---     TAnd t1 t2 -> go t1 ++ go t2
---     TOr
---   undefined -- \case
---   -- TAnd clauses ->
-  --   concatMap cnfCompiler clauses
-  -- TOr terms ->
-  --   concat $ mapM cnfCompiler (toList terms)
-  -- TNot b -> map (map (over _1 not)) $ cnfCompiler b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- | We are currently operating at 31 bits.
+-- type VarId = Int32
+
+-- -- | Negation normal form
+-- data Nnf
+--   = NAnd Nnf Nnf
+--   | NOr Nnf Nnf
+--   | NVar !Bool !Int
+--   | NConst !Bool
+--   deriving (Eq)
+
+-- instance BooleanAlgebra Nnf where
+--   type BooleanVar Nnf = Int
+--   tt = NVar True
+--   ff = NVar False
+
+-- instance Boolean Nnf where
+--   (/\)  = NAnd
+--   (\/)  = NOr
+--   not _ = error "not supported"
+--   true  = NConst True
+--   false = NConst False
+--   {-# INLINE (/\) #-}
+--   {-# INLINE (\/) #-}
+--   {-# INLINE not #-}
+
+-- instance Show Nnf where
+--   showsPrec n = \case
+--     NAnd a b -> showParen (n > 5) (
+--       showsPrec 5 a
+--       . showString " /\\ "
+--       . showsPrec 6 b
+--       )
+--     NOr a b -> showParen (n > 4) (
+--       showsPrec 4 a
+--       . showString " \\/ "
+--       . showsPrec 5 b
+--       )
+--     NVar b i ->
+--       showParen (n > 9) (showString (if b then "tt " else "ff ") . showsPrec 9 i)
+--     NConst b ->
+--       showParen (n > 9) (if b then showString "true" else showString "false")
+
+-- nnfCompiler :: Term Int -> Nnf
+-- nnfCompiler = go True where
+--   go n = \case
+--     TAnd t1 t2 ->
+--       (if n then NAnd else NOr) (go n t1) (go n t2)
+--     TOr  t1 t2 ->
+--       (if n then NOr else NAnd) (go n t1) (go n t2)
+--     TNot t1 ->
+--       go (not n) t1
+--     TVar a ->
+--       NVar n a
+--     TConst b ->
+--       NConst b
+-- {-# INLINE nnfCompiler #-}
+
+-- nnfFlatCompiler :: Term Int -> Nnf
+-- nnfFlatCompiler = flattenNnf . nnfCompiler
+
+-- flattenNnf :: Nnf -> Nnf
+-- flattenNnf = \case
+--   NAnd a b ->
+--     case (flattenNnf a, flattenNnf b) of
+--       (NConst True, t)  -> t
+--       (t, NConst True)  -> t
+--       (NConst False, _) -> NConst False
+--       (_, NConst False) -> NConst False
+--       (a', b') -> NAnd a' b'
+--   NOr a b ->
+--     case (flattenNnf a, flattenNnf b) of
+--       (NConst True, _)  -> NConst True
+--       (_, NConst True)  -> NConst True
+--       (NConst False, t) -> t
+--       (t, NConst False) -> t
+--       (a', b') -> NOr a' b'
+--   x -> x
+
+-- -- | Split a variable into it's components
+-- splitVar :: VarId -> (Bool, Int32)
+-- splitVar i = (not $ testBit i 31, clearBit i 31)
+-- {-# INLINE splitVar #-}
+
+-- -- | Split a variable into it's components
+-- joinVar :: (Bool, Int32) -> VarId
+-- joinVar (b, i) = if b then clearBit i 31 else setBit i 31
+-- {-# INLINE joinVar #-}
+
+-- notvar :: VarId -> VarId
+-- notvar i = complementBit i 31
+-- {-# INLINE notvar #-}
+
+-- -- | A Clause is a unboxed sorted vector of 'Var's.
+-- data Clause = Clause IS.IntSet IS.IntSet
+--   deriving (Eq, Ord)
+
+-- instance BooleanAlgebra (Literal a) where
+--   type BooleanVar (Literal a) = a
+--   tt a = Literal True a
+--   ff a = Literal False a
+
+-- data Literal a =
+--   Literal {-# unpack #-} !Bool {-# unpack #-} !a
+--   deriving (Ord, Eq)
+
+-- instance Show a => Show (Literal a) where
+--   showsPrec n (Literal b a) = showParen (n > 9)
+--     (showString (if b then "tt " else "ff ") . showsPrec 9 a)
+
+-- newtype IntLiteral = IntLiteral (Literal Int)
+--   deriving (Eq, Ord, Num)
+
+-- instance Show IntLiteral where
+--   showsPrec n (IntLiteral (Literal b a)) =
+--     showsPrec n ((if b then 1 else -1) * a)
+
+-- instance BooleanAlgebra Clause where
+--   type BooleanVar Clause = Int
+--   tt a = Clause (IS.singleton a) mempty
+--   ff a = Clause mempty (IS.singleton a)
+
+-- instance Num a => Num (Literal a) where
+--   fromInteger i =
+--     Literal (signum i > 0) (abs $ fromInteger i)
+--   negate (Literal b i) = (Literal (not b) i)
+--   (+) = error "+ is undefined on literals"
+--   (*) = error "* is undefined on literals"
+--   abs (Literal _ i) = Literal True i
+--   signum a = a
+
+-- orVid :: Bool -> Int -> Clause -> Maybe Clause
+-- orVid b i (Clause trs fls)
+--   | b && (not $ i `IS.member` fls ) =
+--     Just $ Clause (i `IS.insert` trs) fls
+--   | not b && (not $ i `IS.member` trs) =
+--     Just $ Clause trs (i `IS.insert` fls)
+--   | otherwise =
+--     Nothing
+
+-- instance Show Clause where
+--   showsPrec n (Clause tr fl) =
+--     showParen (n > 9) $
+--     showString "clause "
+--     . showList (
+--     map (IntLiteral . Literal True) (IS.toList tr)
+--     ++ map (IntLiteral . Literal False) (IS.toList fl)
+--     )
+
+-- clause :: (Foldable f) => f (Bool, Int) -> Clause
+-- clause (toList -> f) =
+--   Clause
+--   (IS.fromList . map snd $ trues)
+--   (IS.fromList . map snd $ falses)
+--   where
+--     (trues, falses) = List.partition fst f
+-- {-# INLINE clause #-}
+
+-- negateClause :: Clause -> [Clause]
+-- negateClause (Clause tr fl) =
+--   [ Clause mempty (IS.singleton i) | i <- IS.toList tr]
+--   ++ [ Clause (IS.singleton i) mempty | i <- IS.toList fl]
+-- {-# INLINE negateClause #-}
+
+-- joinClause :: Clause -> Clause -> Clause
+-- joinClause (Clause trs1 fls1) (Clause trs2 fls2) =
+--   Clause
+--   (IS.union trs1 trs2)
+--   (IS.union fls1 fls2)
+-- {-# INLINE joinClause #-}
+
+-- flattenClause :: Clause -> Maybe Clause
+-- flattenClause a@(Clause tr fl) =
+--   if tr `IS.disjoint` fl
+--   then Just a
+--   else Nothing
+
+-- -- | A CNF is a list of clauses
+-- type Cnf = [Clause]
+
+-- -- instance Boolean Cnf where
+-- --   a /\ b = a ++ b
+
+-- --   (\/) = liftM2 joinClause
+
+-- --   not = \case
+-- --     [] -> false
+-- --     c : rest ->
+-- --      negateClause c \/ not rest
+
+-- --   true  = []
+-- --   false = [clause []]
+
+-- cnfCompiler :: Term Int -> Cnf
+-- cnfCompiler = cnfCompiler' . nnfFlatCompiler
+
+-- cnfCompiler' :: Nnf -> Cnf
+-- cnfCompiler' =
+--   flip appEndo [] . go (Clause mempty mempty) (Endo . (:)) where
+--   -- go: c is the dominating clause. Every clause have to be or'ed with
+--   -- this. This is a fold.
+--   go c k = \case
+--     NAnd t1 t2   -> go c k t1 <> go c k t2
+--     NOr t1 t2    -> go c (\c' -> go c' k t2) t1
+--     NVar b i     ->
+--       case orVid b i c of
+--         Just a -> k a
+--         Nothing -> mempty
+--     NConst True  -> mempty
+--     NConst False -> k c
+
+
+-- -- go where
+-- --   go = \case
+-- --     TAnd t1 t2 -> go t1 ++ go t2
+-- --     TOr
+-- --   undefined -- \case
+-- --   -- TAnd clauses ->
+--   --   concatMap cnfCompiler clauses
+--   -- TOr terms ->
+--   --   concat $ mapM cnfCompiler (toList terms)
+--   -- TNot b -> map (map (over _1 not)) $ cnfCompiler b
--- reduce-util/test/Control/Reduce/BooleanSpec.hs
+++ reduce-util/test/Control/Reduce/BooleanSpec.hs
@@ -3,25 +3,166 @@
 module Control.Reduce.BooleanSpec where
 
 import Control.Reduce.Boolean
+import Prelude hiding (not)
 
 import SpecHelper
 
+-- example1 :: Term Int
+-- example1 =
+--   TAnd
+--   (TAnd
+--     (TConst True)
+--     (TOr
+--       (TNot (TVar 4))
+--       (TAnd
+--         (TAnd
+--           (TOr
+--             (TConst False)
+--             (TAnd (TVar 23) (TConst True)))
+--           (TAnd
+--             (TConst True)
+--             (TAnd
+--               (TConst True)
+--               (TConst True))
+--           )
+--         )
+--         (TAnd
+--          (TConst True)
+--          (TAnd
+--           (TConst True)
+--           (TConst True)
+--          )
+--         )
+--       )
+--     )
+--   )
+--   (TOr (TNot (TVar 4)) (TConst True))
+
+-- example2 :: Term Int
+-- example2 = TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TConst True) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 0)) (TVar 1)) (TVar 1)) (TVar 2)) (TVar 1)) (TVar 2)) (TVar 3)) (TVar 2)) (TVar 3)))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TAnd (TOr (TConst False) (TAnd (TVar 24) (TConst True))) (TAnd (TConst True) (TAnd (TConst True) (TConst True)))))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TAnd (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TOr (TConst False) (TAnd (TVar 27) (TConst True))) (TAnd (TVar 26) (TAnd (TConst True) (TVar 10)))) (TAnd (TVar 25) (TAnd (TAnd (TConst True) (TVar 10)) (TVar 9)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 9)) (TVar 8)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 9)) (TVar 14)) (TVar 11)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 9)) (TVar 14)) (TVar 20)) (TVar 7)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 15)) (TVar 12)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 15)) (TVar 21)) (TVar 11)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 10)) (TVar 15)) (TVar 21)) (TVar 20)) (TVar 7)))) (TAnd (TVar 24) (TAnd (TAnd (TConst True) (TVar 18)) (TVar 12)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TConst True) (TVar 18)) (TVar 21)) (TVar 11)))) (TAnd (TVar 24) (TAnd (TAnd (TAnd (TAnd (TConst True) (TVar 18)) (TVar 21)) (TVar 20)) (TVar 7)))) (TAnd (TVar 24) (TAnd (TAnd (TConst True) (TVar 19)) (TVar 13)))) (TAnd (TVar 24) (TAnd (TAnd (TConst True) (TVar 16)) (TVar 5)))) (TAnd (TVar 24) (TAnd (TAnd (TConst True) (TVar 17)) (TVar 6)))) (TAnd (TConst True) (TAnd (TConst True) (TAnd (TConst True) (TConst True))))))) (TOr (TNot (TVar 4)) (TAnd (TAnd (TOr (TConst False) (TAnd (TVar 22) (TConst True))) (TAnd (TConst True) (TAnd (TConst True) (TConst True)))) (TAnd (TConst True) (TAnd (TConst True) (TConst True)))))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TConst True))) (TOr (TNot (TVar 4)) (TAnd (TOr (TConst False) (TAnd (TVar 28) (TConst True))) (TAnd (TConst True) (TAnd (TConst True) (TAnd (TConst True) (TConst True))))))) (TOr (TNot (TVar 4)) (TAnd (TAnd (TOr (TConst False) (TAnd (TVar 23) (TConst True))) (TAnd (TConst True) (TAnd (TConst True) (TConst True)))) (TAnd (TConst True) (TAnd (TConst True) (TConst True)))))) (TOr (TNot (TVar 4)) (TConst True))
+
 spec :: Spec
 spec = do
-  describe "cnf compiler" $ do
-    it "can compile a simple term" $ do
-      cnfCompiler (
-        TVar 1
-        /\ TVar 2
-        /\ ( neg (TVar 3) \/ TVar 4 )
-        )
-      `shouldBe`
-        [ clause [(True, 1)], clause [(True, 2)], clause [(False, 3), (True, 4)]]
+  describe "term" $ do
+    it "should handle equality" $ do
+      (tt 1 /\ tt 2 :: Term Int) `shouldBe`
+       (tt 1 /\ tt 2)
 
-  --   it "can compile a more complex term" $ do
-  --     cnfCompiler (neg (TVar (True, 1) /\ TVar (True, 2) /\ (TVar (False, 3) \/ TVar (True, 4))))
+    it "should print nicely" $ do
+      show (tt 1 ∧ tt 2 ∨ ff 3 :: Term Int) `shouldBe`
+       "tt 1 ∧ tt 2 ∨ not (tt 3)"
+
+  describe "nnf" $ do
+    it "should handle equality" $ do
+      (tt 1 ∧ tt 2 :: Nnf) `shouldBe`
+       (tt 1 ∧ tt 2)
+
+    it "should be able to not" $ do
+      (not (tt 1 ∧ tt 2) :: Nnf) `shouldBe`
+       (ff 1 ∨ ff 2)
+
+    it "should enable compilation" $ do
+      (crossCompiler $ neg (tt 1 ∧ tt 2 :: Term Int)) `shouldBe`
+       (ff 1 \/ ff 2 :: Nnf)
+
+
+  -- describe "cnf compiler" $ do
+  --   it "can compile a simple term" $ do
+  --     cnfCompiler (
+  --       TVar 1
+  --       /\ TVar 2
+  --       /\ ( neg (TVar 3) \/ TVar 4 )
+  --       )
   --     `shouldBe`
   --       [ clause [(True, 1)], clause [(True, 2)], clause [(False, 3), (True, 4)]]
 
+  --   it "can compile this monster" $ do
+  --     cnfCompiler example1 `shouldBe`
+  --       [clause [(True, 23), (False, 4)]]
+
+  --   it "can compile this expression" $ do
+  --     cnfCompiler (TVar 1 \/ TVar 2 \/ TVar 3 \/ TVar 4) `shouldBe`
+  --       [clause [(True,1),(True,2),(True,3),(True,4)]]
+
+  --   it "can compile this expression" $ do
+  --     cnfCompiler (neg $ TVar 1 /\ TVar 2 /\ TVar 3 /\ TVar 4) `shouldBe`
+  --       [clause [(False,1),(False,2),(False,3),(False,4)]]
+
+  --   it "can compile this expression" $ do
+  --     let expr = (forall [1, 2] TVar \/ forall [2,3] (neg . TVar))
+  --     nnfFlatCompiler expr
+  --       `shouldBe` tt 1 /\ tt 2 \/ ff 2 /\ ff 3
+
+  --     cnfCompiler expr `shouldBe`
+  --       [ clause [(True,1),(False,2)],clause [(True,1),(False,3)],clause [(True,2),(False,3)] ]
+
+  --   it "can compile the bigest one" $ do
+  --     let
+  --       expr =
+  --           (ff 4 \/
+  --            tt 0 /\ tt 1 /\ tt 1 /\ tt 2 /\ tt 1 /\ tt 2 /\ tt 3 /\ tt 2 /\ tt 3
+  --           )
+  --           /\ (ff 4 \/ tt 24)
+  --           /\ (ff 4 \/
+  --               (tt 27
+  --                \/ tt 26 /\ tt 10
+  --                \/ tt 25 /\ (tt 10 /\ tt 9)
+  --                \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 8)
+  --               --  \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 14 /\ tt 11)
+  --               --  \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 14 /\ tt 20 /\ tt 7)
+  --               --  \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 12)
+  --               --  \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 21 /\ tt 11)
+  --               --  \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 21 /\ tt 20 /\ tt 7)
+  --               --  \/ tt 24 /\ (tt 18 /\ tt 12)
+  --               --  \/ tt 24 /\ (tt 18 /\ tt 21 /\ tt 11)
+  --               --  \/ tt 24 /\ (tt 18 /\ tt 21 /\ tt 20 /\ tt 7)
+  --               --  \/ tt 24 /\ (tt 19 /\ tt 13)
+  --               --  \/ tt 24 /\ (tt 16 /\ tt 5)
+  --               --  \/ tt 24 /\ (tt 17 /\ tt 6)
+  --               -- ))
+  --               ))
+  --           /\ (ff 4 \/ tt 22)
+  --           /\ (ff 4 \/ tt 28)
+  --           /\ (ff 4 \/ tt 23)
+
+  --     length (cnfCompiler' expr) `shouldBe` 37
+
+  -- describe "nnf compiler" $ do
+  --   it "can compile and flatten example1" $ do
+  --     flattenNnf (nnfCompiler example1) `shouldBe`
+  --       NOr (NVar False 4) (NVar True 23)
+
+  --   it "can compile and flatten example2" $ do
+  --     flattenNnf (nnfCompiler example2) `shouldBe`
+  --       (ff 4 \/
+  --        tt 0 /\ tt 1 /\ tt 1 /\ tt 2 /\ tt 1 /\ tt 2 /\ tt 3 /\ tt 2 /\ tt 3
+  --       )
+  --       /\ (ff 4 \/ tt 24)
+  --       /\ (ff 4 \/
+  --           (tt 27
+  --            \/ tt 26 /\ tt 10
+  --            \/ tt 25 /\ (tt 10 /\ tt 9)
+  --            \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 8)
+  --            \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 14 /\ tt 11)
+  --            \/ tt 24 /\ (tt 10 /\ tt 9 /\ tt 14 /\ tt 20 /\ tt 7)
+  --            \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 12)
+  --            \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 21 /\ tt 11)
+  --            \/ tt 24 /\ (tt 10 /\ tt 15 /\ tt 21 /\ tt 20 /\ tt 7)
+  --            \/ tt 24 /\ (tt 18 /\ tt 12)
+  --            \/ tt 24 /\ (tt 18 /\ tt 21 /\ tt 11)
+  --            \/ tt 24 /\ (tt 18 /\ tt 21 /\ tt 20 /\ tt 7)
+  --            \/ tt 24 /\ (tt 19 /\ tt 13)
+  --            \/ tt 24 /\ (tt 16 /\ tt 5)
+  --            \/ tt 24 /\ (tt 17 /\ tt 6)
+  --           ))
+  --       /\ (ff 4 \/ tt 22)
+  --       /\ (ff 4 \/ tt 28)
+  --       /\ (ff 4 \/ tt 23)
+
+  -- --   it "can compile a more complex term" $ do
+  -- --     cnfCompiler (neg (TVar (True, 1) /\ TVar (True, 2) /\ (TVar (False, 3) \/ TVar (True, 4))))
+  -- --     `shouldBe`
+  -- --       [ clause [(True, 1)], clause [(True, 2)], clause [(False, 3), (True, 4)]]
+
   
-  -- return ()
+  -- -- return ()
